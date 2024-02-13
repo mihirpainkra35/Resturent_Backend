b72164ca2b6d4aec9560be238cff819789563df9
mihirpainkra35 <96572058+mihirpainkra35@users.noreply.github.com>
2024-02-13 11:48:58 +0530

    check routes

--- router/routes.js
+++ router/routes.js
@@ -2,23 +2,23 @@
 const express = require('express');
 
 
-const {UserOTPGeneration} = require('../controller/UserControl.module.js') 
-const {UserOTPVerification} = require('../controller/UserControl.module.js') 
+// const {UserOTPGeneration} = require('../controller/UserControl.module.js') 
+// const {UserOTPVerification} = require('../controller/UserControl.module.js') 
 
 const router = express.Router();
 
 // GET Request
-router.route('/user').get((req,res)=>{res.status(201).send('get user data')})
+// router.route('/user').get((req,res)=>{res.status(201).send('get user data')})
 
 // POST Request
-router.route('/user/generateOTP').post(UserOTPGeneration)
-router.route('/user/verifyOTP').post(UserOTPVerification)
+// router.route('/user/generateOTP').post(UserOTPGeneration)
+// router.route('/user/verifyOTP').post(UserOTPVerification)
 
 // PUT Request
-router.route('/user').get((req,res)=>{res.status(201).send('get user data')})
+// router.route('/user').get((req,res)=>{res.status(201).send('get user data')})
 
 // UPDATE Request
-router.route('/user').get((req,res)=>{res.status(201).send('get user data')})
+// router.route('/user').get((req,res)=>{res.status(201).send('get user data')})
 
 
 
