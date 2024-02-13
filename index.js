d90b6a98249fcdc08e71c02f8acb413d827176eb
mihirpainkra35 <96572058+mihirpainkra35@users.noreply.github.com>
2024-02-13 11:14:41 +0530

    testing vercel

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-require('dotenv').config()
+// require('dotenv').config()
 // lib's import
 const express = require('express');
 
 // routes import
-const routes = require('./router/routes.js')
+// const routes = require('./router/routes.js')
 
 // db connection import
-require('./DB/conn.js')
+// require('./DB/conn.js')
 
 const app = express();
 
@@ -16,10 +16,10 @@ const port = process.env.PORT || 3000;
 
 // middleware       
 app.use(express.json());
-app.use('/resturent',routes)
+// app.use('/resturent',routes)
 
 app.get('/',(req,res)=>{
-res.json({message:'hello world'});
+res.send('hello world');
 });
 
 app.listen(port,()=>{
