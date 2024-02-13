cda7f38fd667568801df97c4ac59af99567ad824
mihirpainkra35 <96572058+mihirpainkra35@users.noreply.github.com>
2024-02-13 11:58:44 +0530

    checking mongodb

--- DB/conn.js
+++ DB/conn.js
@@ -5,11 +5,17 @@ const mongoose = require('mongoose');
 // url for mongodb
 const url = process.env.DATABASE_URL 
 
+try {
+    mongoose.connect(url,{});
+    console.log("connected to mongodb");
+} catch (error) {
+    console.log (error);
+}
 
-mongoose.connect(url,{}).then(()=>{
-    console.log("connected to mongodb server");
-}).catch(err => {
-    console.log(err);
-});
+// mongoose.connect(url,{}).then(()=>{
+//     console.log("connected to mongodb server");
+// }).catch(err => {
+//     console.log(err);
+// });
 
 module.exports = mongoose.connection;
