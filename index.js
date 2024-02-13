0256243f123c04b190ef7741183d39a2883a3db8
mihirpainkra35 <96572058+mihirpainkra35@users.noreply.github.com>
2024-02-13 11:24:12 +0530

    routes update

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ require('dotenv').config()
 const express = require('express');
 
 // routes import
-// const routes = require('./router/routes.js')
+const routes = require('./router/routes')
 
 // db connection import
-// require('./DB/conn.js')
+require('./DB/conn')
 
 const app = express();
 
@@ -16,7 +16,7 @@ const port = process.env.PORT || 3000;
 
 // middleware       
 app.use(express.json());
-// app.use('/resturent',routes)
+app.use('/resturent',routes)
 
 app.get('/',(req,res)=>{
 res.send('hello world');
